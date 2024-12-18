dc0c65f0fe40bdd3a47130b250d079e030f48dba
María Millán Gordillo <88489753+MariaMG11@users.noreply.github.com>
2024-12-18 10:16:11 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,24 @@
+# Importar librerias
+import streamlit as st
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
 
+# Cargar datos
+data = pd.read_csv('Renewable_Energy.csv')
+
+# Agrupa los datos por Energy_Type, quédate con los valores que sean Total Renewable y sumalos
+data = data.groupby('Energy_Type').get_group('Total Renewable')
+
+# Encabezado
+st.header('Proyecto de Energias Renovables')
+
+# Título
+st.title('Renewable Energy')
+
+# Subtítulo
+st.write('Datos de energias renovables')
+
+# Mostrar datos
+st.write(data)
