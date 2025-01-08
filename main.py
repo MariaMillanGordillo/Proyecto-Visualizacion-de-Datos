3e1b9835a0140077c1b0a8e535d810d3ce2cb280
mclinaresvazquez <mclinaresvazquez@al.uloyola.es>
2025-01-08 21:10:57 +0100

    Add files via upload

--- main.py
+++ main.py
@@ -8,45 +8,40 @@ import seaborn as sns
 # Cargar datos
 data = pd.read_csv('Renewable_Energy.csv')
 
-# Encabezado
-st.header('Proyecto de Energias Renovables')
+# Dividir el dataset en dos según la columna 'Indicator'
+grouped_data = data.groupby('Indicator')
 
-# Título
-st.title('Renewable Energy')
+# Crear dos datasets separados
+generation = grouped_data.get_group('Electricity Generation')
+capacity = grouped_data.get_group('Electricity Installed Capacity')
 
-# Subtítulo
-st.write('Datos de energias renovables')
+# Eliminar las columnas especificadas de ambos datasets
+columns_to_drop = ['Source', 'CTS_Name', 'CTS_Code', 'CTS_Full_Descriptor', 'Indicator']
+generation.drop(columns=columns_to_drop, inplace=True)
+capacity.drop(columns=columns_to_drop, inplace=True)
 
-# Mostrar datos
-st.write(data)
+# Crear un desplegable en Streamlit para seleccionar el país
+st.subheader('Generación de energía por territorio según el tipo de energía')
+paises = data['Country'].unique().tolist()
+pais_seleccionado = st.selectbox('Selecciona un país', paises)
 
-# Seleccionar un rango de años
-years = st.slider('Años', 2000, 2023, (2000, 2023))
+# Filtrar los datos para el país seleccionado
+datos_pais = generation[generation['Country'] == pais_seleccionado]
 
-# Filtrar las columnas por el rango de años seleccionado
-selected_columns = [f'F{year}' for year in range(years[0], years[1] + 1)]
-filtered_data = data[['Technology'] + selected_columns]
+# Seleccionar el rango de años
+years = [f'F{year}' for year in range(2000, 2023)]
+year_range = st.slider('Selecciona el rango de años', 2000, 2022, (2000, 2022))
+years_selected = [f'F{year}' for year in range(year_range[0], year_range[1] + 1)]
 
-# Agrupar los datos por tecnología y sumar los valores totales para el rango de años seleccionado
-technology_distribution = filtered_data.groupby('Technology').sum()
+# Generar gráficos en Streamlit
+for _, fila in datos_pais.iterrows():
+    energia = fila[years_selected]
+    tecnologia = fila['Technology']
+    st.write(f'Electricity Generation (GWh) in {pais_seleccionado} - {tecnologia}')
+    st.bar_chart(energia)
+    
 
-# Sumar los valores totales por tecnología a través de los años
-technology_distribution['Total'] = technology_distribution.sum(axis=1)
-technology_distribution = technology_distribution.sort_values(by='Total', ascending=False)
 
-# Crear el gráfico de barras
-st.subheader(f'Cantidad de Energía Generada por Tecnología ({years[0]}-{years[1]})')
-fig, ax = plt.subplots(figsize=(12, 8))
-technology_distribution['Total'].plot(kind='bar', color='skyblue', edgecolor='black', ax=ax)
 
-# Personalizar el gráfico
-ax.set_title(f"Cantidad Total de Energía Generada por Tecnología ({years[0]}-{years[1]})", fontsize=14)
-ax.set_xlabel("Tecnología", fontsize=12)
-ax.set_ylabel("Cantidad Total de Energía (GWh)", fontsize=12)
-ax.tick_params(axis='x', rotation=45, labelsize=10)
-ax.grid(axis='y', linestyle='--', alpha=0.7)
 
-# Mostrar el gráfico en Streamlit
-st.pyplot(fig)
 
-st.write('La tecnología que más energía ha generado a lo largo de los años es la hidroeléctrica, seguida de la eólica y la solar.')
